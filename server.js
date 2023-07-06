13bc6f26e2dd1c4bda11e17c8365309f08da58cd
kwonhygge <cleancode.kwon@gmail.com>
2023-07-06 23:24:53 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+app.post("/translate", async (req, res) => {
+    const url = "https://deepl-translator.p.rapidapi.com/translate";
+
+    try {
+        const { text, source, target } = req.body;
+        const data = {
+            text: text,
+            source: source,
+            target: target,
+        };
+
+        console.log(data, 'data to send')
+
+        const response = await axios.post(url, data, {
+            headers: {
+                "Content-Type": "application/json",
+                "X-RapidAPI-Key": req.body["X-RapidAPI-Key"],
+                "X-RapidAPI-Host": "deepl-translator.p.rapidapi.com",
+            },
+        });
+
+        const responseData = response.data;
+        console.log(responseData);
+        res.json(responseData);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "오류가 발생했습니다." });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`서버가 포트 ${port}에서 실행 중입니다.`);
+});
